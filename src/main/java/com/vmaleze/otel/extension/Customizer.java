0849da4b01842bc6c2d721c6642f2dc19cd3e674
Vivien Maleze <vmaleze@gmail.com>
2023-04-13 16:07:31 +0200

    Init repo

---
+++ src/main/java/com/vmaleze/otel/extension/Customizer.java
@@ -0,0 +1,27 @@
+package com.vmaleze.otel.extension;
+
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+
+/**
+ * Note this class is wired into SPI via
+ * {@code resources/META-INF/services/io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider}
+ */
+public class Customizer implements AutoConfigurationCustomizerProvider {
+
+  @Override
+  public void customize(AutoConfigurationCustomizer autoConfiguration) {
+    // Set the sampler to be the default parentbased_always_on, but drop calls to health endpoints
+    autoConfiguration.addTracerProviderCustomizer(
+        (sdkTracerProviderBuilder, configProperties) ->
+            sdkTracerProviderBuilder.setSampler(
+                Sampler.parentBased(
+                    RuleBasedRoutingSampler.builder(SpanKind.SERVER, Sampler.alwaysOn())
+                        .drop(SemanticAttributes.HTTP_TARGET, "*health*")
+                        .build())));
+  }
+}
