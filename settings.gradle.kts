1b6ffda0210cc3e83a6a3107c9e42cf834ffb034
ttrabelsi <Lerentis@users.noreply.github.com>
2026-03-05 11:49:33 +0100

    chore(): update dependencies to latest

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,10 @@
 pluginManagement {
     plugins {
-        id("org.springframework.boot") version "3.0.5"
-        id("io.spring.dependency-management") version "1.1.0"
-        id("de.undercouch.download") version "5.4.0"
-        id("com.github.johnrengelman.shadow") version "8.1.1"
-        id("com.google.cloud.tools.jib") version "3.3.2"
+        id("org.springframework.boot") version "3.5.11"
+        id("io.spring.dependency-management") version "1.1.7"
+        id("de.undercouch.download") version "5.7.0"
+        id("com.gradleup.shadow") version "9.3.2"
+        id("com.google.cloud.tools.jib") version "3.5.2"
     }
 }
 
